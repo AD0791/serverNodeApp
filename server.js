0a505ed4c8af03c91b11af2f8ddf652bbac78164
Alexandro Disla <alexandrodisla@hotmail.com>
2020-08-10 15:35:15 -0400

    crud is done

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ const mongoose = require("mongoose");
 
 let app = express();
 
-
 // access the schema
 let Data = require("./noteSchema");
-
 
 // mongodb connection
 mongoose.connect("mongodb://localhost/noteDB", {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify: false
 });
 
-mongoose.connection.once("open", () => {
-  return console.log("connect to DB");
-}).on("error", (error) => {
-  return console.log(`Failed to connect ${error}`);
-});
-
+mongoose.connection
+  .once("open", () => {
+    return console.log("connect to DB");
+  })
+  .on("error", (error) => {
+    return console.log(`Failed to connect ${error}`);
+  });
 
 // controller situation with the server
 //
@@ -29,31 +29,52 @@ app.post("/create", (req, res) => {
   let note = new Data({
     title: req.get("title"),
     date: req.get("date"),
-    note: req.get("note")
+    note: req.get("note"),
   });
 
   note.save().then(() => {
     if (note.isNew === false) {
-      console.log('Saved Data!');
+      console.log("Saved Data!");
       // iphone will know
       res.send("Saved data");
     } else {
-      console.log('failed to save data');
+      console.log("failed to save data");
     }
   });
-
 });
+
 // delete note (Delete request)
+app.post('/delete', (req, res) => {
+  Data.findOneAndRemove({
+    _id: req.get('id')
+  }, (err) => {
+    console.log(`failed ${err}`)
+  });
+  res.send("Deleted!");
+});
+
 
 // update note (put or patch request)
+app.post('/update', (req, res) => {
+  Data.findOneAndUpdate({
+    _id: req.get("id")
+  }, {
+    title: req.get("title"),
+    date: req.get("date"),
+    note: req.get("note")
+  }, (err) => {
+    console.log(`Failed to Update ${err}`);
+  });
+  res.send("Updated!");
+});
+
 
 // fetch one or all notes ( get request)
-
-
-
-
-
-
+app.get('/fetch', (req, res) => {
+  Data.find({}).then((DBitems) => {
+    res.send(DBitems);
+  });
+});
 
 
 
@@ -83,5 +104,5 @@ app.post("/create", (req, res) => {
 // server port
 // http://172.20.10.9:8081/create
 app.listen(8081, () => {
-  console.log('Server is running');
+  console.log("Server is running");
 });
